876d9dc75e34924f83ad7a3b8b757cac0bc2a4ee
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2020-12-26 15:48:47 +0100

    Correct some css

--- utils.jl
+++ utils.jl
@@ -14,10 +14,10 @@ function hfun_fill_profile()
         </div>
         """
         s *= "<div>"
-        s *= "<b> $(member[:name]) }} </b> <br> $(member[:institution]) }} <br>"
-        s *= haskey(member, :website) ? """<a style="white-space: nowrap" href=$(member[:website]) <i class="fa fa-home"></i> </a>""" : ""
+        s *= "<b> $(member[:name]) </b> <br> $(member[:institution]) <br>"
+        s *= haskey(member, :website) ? """<a style="white-space: nowrap" href=$(member[:website])> <i class="fa fa-home"></i> </a>""" : ""
         s *= haskey(member, :github) ? """<a style="white-space: nowrap" href="https://github.com/$(member[:github])"> <i class="fa fa-github"></i> </a>""" : ""
-        s *= haskey(member,  :twitter) ? """<a style="white-space: nowrap" href="https://twitter.com/$(member[:twitter])"> <i class="fa fa-twitter"></i>  </a>""" : ""
+        s *= haskey(member, :twitter) ? """<a style="white-space: nowrap" href="https://twitter.com/$(member[:twitter])"> <i class="fa fa-twitter"></i>  </a>""" : ""
         s *= haskey(member, :email) ? """<a style="white-space: nowrap" href="mailto:$(member[:email])"> <i class="fa fa-envelope"></i> </a>""" : ""
         s *= haskey(member, :linkedin) ? """<a style="white-space: nowrap" href="https://www.linkedin.com/in/$(member[:linkedin])"> <i class="fa fa-linkedin"></i> </a>""" : ""
         s *= "</div>"
