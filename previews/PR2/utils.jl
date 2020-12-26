ff3c07c4bf0f7c9bfeed7fe95c03a0edb70ed72c
theogf <theogf@users.noreply.github.com>
2020-12-26 15:40:30 +0000

    Deploying to gh-preview from  @ 3bae40f06ffe0c0e280858050f65de22c586462c 🚀

--- previews/PR2/utils.jl
+++ previews/PR2/utils.jl
@@ -9,18 +9,25 @@ function hfun_fill_profile()
     s = ""
     for member in team
         s *= """
-        <div class="team-row-image">
-        <img src="$(member[:photo])" alt="$(member[:name])">
-        </div>
+        <div class="team-row"> 
+            <div class="team-row-image">
+            <img src="$(member[:photo])" alt="$(member[:name])">
+            </div>
         """
-        s *= "<div>"
-        s *= "<b> $(member[:name]) }} </b> <br> $(member[:institution]) }} <br>"
-        s *= haskey(member, :website) ? """<a style="white-space: nowrap" href=$(member[:website]) <i class="fa fa-home"></i> </a>""" : ""
-        s *= haskey(member, :github) ? """<a style="white-space: nowrap" href="https://github.com/$(member[:github])"> <i class="fa fa-github"></i> </a>""" : ""
-        s *= haskey(member,  :twitter) ? """<a style="white-space: nowrap" href="https://twitter.com/$(member[:twitter])"> <i class="fa fa-twitter"></i>  </a>""" : ""
-        s *= haskey(member, :email) ? """<a style="white-space: nowrap" href="mailto:$(member[:email])"> <i class="fa fa-envelope"></i> </a>""" : ""
-        s *= haskey(member, :linkedin) ? """<a style="white-space: nowrap" href="https://www.linkedin.com/in/$(member[:linkedin])"> <i class="fa fa-linkedin"></i> </a>""" : ""
-        s *= "</div>"
+        s *= """<div class="team-row-bio">"""
+        s *= """<div class="team-row-bio-links">"""
+        s *= "<b> $(member[:name]) </b> <br> $(member[:institution]) <br>"
+        s *= haskey(member, :website) ? """<a style="white-space: nowrap" href=$(member[:website])> <i class="fas fa-home"></i> </a>""" : ""
+        s *= haskey(member, :github) ? """<a style="white-space: nowrap" href="https://github.com/$(member[:github])"> <i class="fab fa-github"></i> </a>""" : ""
+        s *= haskey(member, :twitter) ? """<a style="white-space: nowrap" href="https://twitter.com/$(member[:twitter])"> <i class="fab fa-twitter"></i>  </a>""" : ""
+        s *= haskey(member, :email) ? """<a style="white-space: nowrap" href="mailto:$(member[:email])"> <i class="fas fa-envelope"></i> </a>""" : ""
+        s *= haskey(member, :linkedin) ? """<a style="white-space: nowrap" href="https://www.linkedin.com/in/$(member[:linkedin])"> <i class="fab fa-linkedin"></i> </a>""" : ""
+        s *= "</div>" # t
+        s *= "</div>" # team-row-bio-links
+        s *= "<br>"
+        s *= "</div>" # team-row
+        s *= "<hr>"
     end
+    s *= ""
     return s
 end
