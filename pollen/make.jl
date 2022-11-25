fbbb8754cad17014e85496a654b4b651aa1e2571
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2022-11-25 21:35:09 +0100

    Add moar

--- pollen/make.jl
+++ pollen/make.jl
@@ -4,9 +4,9 @@ Pkg.add(Pkg.PackageSpec(url="https://github.com/lorenzoh/Pollen.jl"))
 using Pollen
 using KernelFunctions
 
-dir = make
+dir = normpath(joinpath(@__DIR__, ".."))
 config = Pollen.PollenPlugin()
-Pollen.setup_docs_project(dir, config)
+Pollen.setup_docs_project(dir, config; force=true)
 Pollen.setup_docs_files(dir, config)
 Pollen.setup_docs_actions(dir, config)
 
---
+++ src/JuliaGPsWebsite.jl
@@ -0,0 +1,2 @@
+module JuliaGPsWebsite
+end
